ca5864c80e9ef8f897133f033dbe12623c0548e7
q66 <daniel@octaforge.org>
2020-07-20 01:23:39 +0200

    patch script can now apply out of tree

--- patch_libressl.sh
+++ patch_libressl.sh
@@ -13,26 +13,31 @@ if [ ! -x "$(command -v patch)" ]; then
     die "patch not found"
 fi
 
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
 if [ ! -d "$1" ]; then
     die "libressl not found"
 fi
 
 echo "copying build files..."
 
-cp crypto/Makefile.am.* "$1"/crypto || die "failed to copy makefiles"
-cp crypto/*.c crypto/*.h "$1"/crypto || die "failed to copy source files"
+cp ${SCRIPT_DIR}/crypto/Makefile.am.* "$1"/crypto || \
+    die "failed to copy makefiles"
+cp ${SCRIPT_DIR}/crypto/*.c ${SCRIPT_DIR}/crypto/*.h "$1"/crypto || \
+    die "failed to copy source files"
 
 echo "copying generated files..."
 
-for x in $(find generated -name '*.S'); do
-    x=${x#generated/}
+for x in $(find "${SCRIPT_DIR}/generated" -name '*.S'); do
+    x=${x#${SCRIPT_DIR}/generated/}
     mkdir -p "$1"/${x%/*} || die "failed to create ${x%/*}"
-    cp generated/"$x" "$1"/$x || die "failed to copy $x"
+    cp ${SCRIPT_DIR}/generated/"$x" "$1"/$x || die "failed to copy $x"
 done
 
 echo "patching tree..."
 
-for p in patches/*.patch; do
+for p in ${SCRIPT_DIR}/patches/*.patch; do
     patch -d "$1" -Np1 < $p || die "failed to apply $p"
 done
 
